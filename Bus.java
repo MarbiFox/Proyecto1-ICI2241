be37100dd7510a9346e0c0fe431f49773926726b
Thomas Molina <101366318+EDANHS@users.noreply.github.com>
2022-09-02 21:37:12 -0400

    Archivos finales

--- Bus.java
+++ Bus.java
@@ -1,20 +1,81 @@
 package com.mycompany.proyecto1.prograav;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Bus {
     //Variables de instancia
-    private String nombre;
-    private int fecha;
+    private String nombre_bus;
+    private String destino;
+    private String fecha;
     private int costo;
     private int num_asientos;
     
     //opcional
-    private boolean lleno;
-    private ArrayList arrayAsientos;
+    //private boolean lleno;
+    //private ArrayList arrayAsientos;
     
-    //----Metodos----
-    public String get_nombre(){
-        return this.nombre;
+    //----Constructor----  
+    public Bus(String nombre, String fecha, int costo, int num_asientos,String destino){
+        this.nombre_bus = nombre;
+        this.fecha = fecha;
+        this.costo = costo;
+        this.num_asientos = num_asientos;
+        this.destino = destino;
+        //opcional
+        //this.lleno = lleno;
+        //arrayAsientos = new ArrayList();
     }
     
+    //----Metodos----
+        
+    //---Getter----
+    public String get_nombre(){
+        return this.nombre_bus;
+    }
+    
+    public String get_destino(){
+        return this.destino;
+    }
+    
+    public String get_fecha(){
+        return this.fecha;
+    }
+    
+    public int get_costo(){
+        return this.costo;
+    }
+    
+    public int get_numAsientos(){
+        return this.num_asientos;
+    }
+    
+    //----Setter----
+    
+    public void set_nombre(String nombre_bus){
+        this.nombre_bus = nombre_bus;
+    }
+       
+    public void set_destino(String destino){
+        this.destino = destino;
+    }
+    
+    public void set_fecha(String fecha){
+        this.fecha = fecha;
+    }
+    
+    public void set_costo(int costo){
+        this.costo = costo;
+    }
+    
+    public void set_numAsientos(int num_asientos){
+        this.num_asientos = num_asientos;
+    }
+    
+    public void mostrar_informacion_bus(){
+        System.out.println("----"+this.nombre_bus+"----");
+        System.out.println("Destino: "+this.destino);
+        System.out.println("Fecha: "+this.fecha);
+        System.out.println("Precio: "+this.costo);
+        System.out.println("N° asientos: "+this.num_asientos);
+        System.out.println("---------------------------");
+    }
 }
--- Ciudad.java
+++ Ciudad.java
@@ -4,42 +4,47 @@ import java.util.ArrayList;
 
 public class Ciudad {
     //Variables de instancia
-    private String nombre;
+    
+    private String nombre; //ciudad de partida
+    private int cantidad_buses;
     private ArrayList<Bus> buses;
     
-    //----Constructores----  
-    
+    //----Constructor----  
     /*Crea una ciudad con un nombre e inicializa el ArrayList*/
     public Ciudad(String n){
         this.nombre = n;
         this.buses = new ArrayList();
+        this.cantidad_buses = 0;
     }
-    
-    /*Crea una ciudad con "none" como nombre e inicializa el ArrayList*/
-    public Ciudad(){
-        this.nombre = "none";
-        this.buses = new ArrayList();
-    }
-    
+       
     //----Metodos----
     
+    //----Setter----
     /*Modifica el nombre de la ciudad*/
     public void set_nombre(String n){
         this.nombre = n;
     }
     
+    public void set_cantidad_de_buses(int c){
+        this.cantidad_buses = c;
+    }
+    
+    //----Getter----
     /*Obtiene el nombre de la ciudad*/
     public String get_nombre(){
         return this.nombre;
     }
     
-    /*Agrega un nuevo elemento a la ArrayList*/
-    public void agregar(Bus p){
-        this.buses.add(p);
+    public int getSizeArrayList(){
+        return this.cantidad_buses;
     }
-      
-    /*Obtiene un bus de la ciudad a partir de un nombre*/
     
+    public ArrayList getArrayList(){
+        ArrayList k = this.buses;
+        return k;
+    }
+       
+    /*Obtiene un bus de la ciudad a partir de un nombre*/
     public Bus obtenerBus(String var){
         int i;
         for(i = 0;i < this.buses.size();i++){
@@ -49,6 +54,29 @@ public class Ciudad {
             }
         }
         return null;
+    }
+    
+    /*obtiene un nuevo elemento a partir de un nombre y una fecha*/
+    public Bus obtenerBus(String nombre, String fecha){
+        int i;
+        for(i = 0;i < this.buses.size();i++){
+            Bus datos = this.buses.get(i);
+            if(datos.get_nombre().equals(nombre) && datos.get_fecha().equals(fecha)){
+                return this.buses.get(i);
+            }
+        }
+        return null;
+    }
+    
+    /*Agrega un nuevo elemento a la ArrayList*/
+    public void agregarBus(Bus p){
+        this.buses.add(p);
+        this.cantidad_buses++;
+    }
+    
+    public void agregarBus(Bus p,int valor_actual){
+        this.buses.add(p);
+        this.cantidad_buses += valor_actual;
     }
     
 }
